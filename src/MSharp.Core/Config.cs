27055b7244055bd3cbc7e08deb0454f67a6ccac1
Marihachi <marihachi0620@gmail.com>
2016-03-19 00:56:06 +0900

    change to property

--- src/MSharp.Core/Config.cs
+++ src/MSharp.Core/Config.cs
@@ -4,7 +4,7 @@ namespace MSharp.Core
 {
 	public class Config
 	{
-		public string SessionKeyName = "hmsk";
+		public string SessionKeyName { get; set; } = "hmsk";
 		public Uri Url { get; set; } = new Uri("https://misskey.xyz");
 		public Uri LoginUrl { get; set; } = new Uri("https://login.misskey.xyz");
 		public Uri ApiUrl { get; set; } = new Uri("https://himasaku.misskey.xyz");
