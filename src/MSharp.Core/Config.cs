98a9295a9cd64aef1c4603f2ebad2ebc1d297a15
Marihachi <marihachi0620@gmail.com>
2016-06-12 09:01:21 +0900

    fix urls (reverted from commit 6bcf32242cb5c527d284b1cec811956c55e9645f)

--- src/MSharp.Core/Config.cs
+++ src/MSharp.Core/Config.cs
@@ -5,10 +5,10 @@ namespace MSharp.Core
 	public static class Config
 	{
 		public static string SessionKeyName { get; set; } = "hmsk";
-		public static Uri Url { get; set; } = new Uri("https://misskey.link");
-		public static Uri LoginUrl { get; set; } = new Uri("https://login.misskey.link");
-		public static Uri ApiUrl { get; set; } = new Uri("https://himasaku.misskey.link/ ");
-		public static Uri NonLoginApiUrl { get; set; } = new Uri("https://api.misskey.link");
-		public static Uri StreamingApiUrl { get; set; } = new Uri("https://himasaku.misskey.link:3000");
+		public static Uri Url { get; set; } = new Uri("https://misskey.xyz");
+		public static Uri LoginUrl { get; set; } = new Uri("https://login.misskey.xyz");
+		public static Uri ApiUrl { get; set; } = new Uri("https://himasaku.misskey.xyz");
+		public static Uri NonLoginApiUrl { get; set; } = new Uri("https://api.misskey.xyz");
+		public static Uri StreamingApiUrl { get; set; } = new Uri("https://himasaku.misskey.xyz:3000");
 	}
 }
